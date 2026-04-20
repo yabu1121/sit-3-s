ee8f9aa47e0e9078c8ce4d87fa8722799304430d
yabu1121 <n.hayato0122@outlook.jp>
2026-04-20 13:53:06 +0900

    hello

--- java/Hello.java
+++ java/Hello.java
@@ -4,14 +4,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 class Hello {
-  public static void main(String[] args) throws IOException{
-    Path path = Paths.get("sample.txt");
-    try {
-      String content = Files.readString(path);
-      System.out.println(content);
-    } catch (Exception e) {
-      println("")
-
+    public static void main(String[] args) {
+        Path path = Paths.get("sample.txt");
+        try {
+            String content = Files.readString(path);
+            System.out.println(content);
+        } catch (IOException e) {
+            System.err.println("エラー：ファイルを読み込めませんでした。");
+            System.err.println("ファイルが存在するか確認してください: " + path.toAbsolutePath());
+        }
     }
-  }
 }
